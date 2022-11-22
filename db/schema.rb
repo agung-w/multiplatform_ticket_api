b744c7dee1042386d04ed5955c3325926610fc22
agung-w <suronokz3471@yahoo.com>
2022-11-22 23:23:41 +0700

    Implement omniauth on model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_154216) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_160657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "credentials", force: :cascade do |t|
+    t.string "access_token"
+    t.string "refresh_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
