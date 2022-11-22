4b0b748373bf8391c7972c06cec30446f56741d9
agung-w <suronokz3471@yahoo.com>
2022-11-22 23:54:02 +0700

    Generate wallet endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_160657) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_164901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,4 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_160657) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "wallets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "balance", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "wallets", "users"
 end
