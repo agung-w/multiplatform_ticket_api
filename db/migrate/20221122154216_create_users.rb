c1b3785d6aa2e964b10782f03ad5cd5fc25e8386
agung-w <suronokz3471@yahoo.com>
2022-11-22 22:43:31 +0700

    Generate model user

---
+++ db/migrate/20221122154216_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :phone
+      t.string :reset_password_token
+      t.timestamp :reset_password_at
+
+      t.timestamps
+    end
+  end
+end
