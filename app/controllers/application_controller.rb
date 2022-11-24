b24b7d019460ba80924e2a5f7ff7622c8631a58a
agung-w <suronokz3471@yahoo.com>
2022-11-24 17:49:24 +0700

    Change error response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 class ApplicationController < ActionController::Base
     include ActionController::HttpAuthentication::Token
-    def not_found
-        render json: { error: 'not_found' }
-    end
     def authorize_request
         token,_options=token_and_options(request)
         decoded=AuthenticationTokenService.decode(token)
         @current_user=User.find(decoded['user_id'])
-        rescue ActiveRecord::RecordNotFound,JWT::DecodeError
-            render json:{ error: 'not_found' }, status: :unauthorized   
+
+        rescue ActiveRecord::RecordNotFound,JWT::DecodeError => e
+            render json:{ 
+                error: {
+                    status: 401,
+                    message:e.to_s
+                }
+            },status: :unauthorized
     end
 end
 
